2481d0bedd7ed552e6383bd9782fcf9c00991ddd
Josiah Roa <josiahroa18@gmail.com>
2020-03-10 12:23:45 -0600

    Added form functionality to look up other github users

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,88 +1,111 @@
 import React from 'react';
 import axios from 'axios';
 import UserCard from './components/UserCard';
+import UserForm from './components/UserForm';
 import './App.css';
 
 class App extends React.Component{
   constructor(){
     super();
     this.state={
-      users: {
-        "login": "josiahroa18",
-        "id": 35950476,
-        "node_id": "MDQ6VXNlcjM1OTUwNDc2",
-        "avatar_url": "https://avatars0.githubusercontent.com/u/35950476?v=4",
-        "gravatar_id": "",
-        "url": "https://api.github.com/users/josiahroa18",
-        "html_url": "https://github.com/josiahroa18",
-        "followers_url": "https://api.github.com/users/josiahroa18/followers",
-        "following_url": "https://api.github.com/users/josiahroa18/following{/other_user}",
-        "gists_url": "https://api.github.com/users/josiahroa18/gists{/gist_id}",
-        "starred_url": "https://api.github.com/users/josiahroa18/starred{/owner}{/repo}",
-        "subscriptions_url": "https://api.github.com/users/josiahroa18/subscriptions",
-        "organizations_url": "https://api.github.com/users/josiahroa18/orgs",
-        "repos_url": "https://api.github.com/users/josiahroa18/repos",
-        "events_url": "https://api.github.com/users/josiahroa18/events{/privacy}",
-        "received_events_url": "https://api.github.com/users/josiahroa18/received_events",
-        "type": "User",
-        "site_admin": false,
-        "name": "Josiah Roa",
-        "company": "Lambda School",
-        "blog": "josiahroa.com",
-        "location": "Denver, CO",
-        "email": null,
-        "hireable": null,
-        "bio": "Passionate Web Developer",
-        "public_repos": 52,
-        "public_gists": 0,
-        "followers": 36,
-        "following": 38,
-        "created_at": "2018-01-30T06:50:23Z",
-        "updated_at": "2020-03-10T03:27:52Z"
-    },
+      mainUserName: 'josiahroa18',
+      mainUser: {},
       followers: []
     }
   }
 
-  // componentDidMount(){
-  //   // My Github Data
-  //   axios.get('https://api.github.com/users/josiahroa18')
-  //   .then(res => {
-  //     this.setState({
-  //       users: res.data
-  //     })
-  //   })
-  //   .catch(err => {
-  //     console.log(err);
-  //   })
+  componentDidMount(){
+    // My Github Data
+    axios.get(`https://api.github.com/users/${this.state.mainUserName}`)
+    .then(res => {
+      this.setState({
+        mainUser: res.data
+      })
+    })
+    .catch(err => {
+      console.log(err);
+    })
 
-  //   // Followers Data
-  //   axios.get('https://api.github.com/users/josiahroa18/followers')
-  //   .then(res => {
-  //     res.data.forEach(user => {
-  //       axios.get(`https://api.github.com/users/${user.login}`)
-  //       .then(res => {
-  //         this.setState({
-  //           followers: [...this.state.followers, res.data]
-  //         })
-  //       })
-  //       .catch(err => {
-  //         console.log(err);
-  //       })
-  //     })
-  //   })
-  //   .catch(err => {
-  //     console.log(err);
-  //   })
-  // }
+    // Followers Data
+    axios.get(`https://api.github.com/users/${this.state.mainUserName}/followers`)
+    .then(res => {
+      res.data.forEach(user => {
+        axios.get(`https://api.github.com/users/${user.login}`)
+        .then(res => {
+          this.setState({
+            followers: [...this.state.followers, res.data]
+          })
+        })
+        .catch(err => {
+          console.log(err);
+        })
+      })
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
+  // Runs when input has changed
+  componentDidUpdate(prevProps, prevState){
+    console.log('Component update');
+    if(prevState.mainUserName !== this.state.mainUserName){
+      // Reset followers state
+      this.setState({
+        followers: []
+      })
+      // Main user data
+      axios.get(`https://api.github.com/users/${this.state.mainUserName}`)
+      .then(res => {
+        this.setState({
+          mainUser: res.data
+        })
+      })
+      .catch(err => {
+        console.log(err);
+      })
+
+      // Followers Data
+      axios.get(`https://api.github.com/users/${this.state.mainUserName}/followers`)
+      .then(res => {
+        res.data.forEach(user => {
+          axios.get(`https://api.github.com/users/${user.login}`)
+          .then(res => {
+            this.setState({
+              followers: [...this.state.followers, res.data]
+            })
+          })
+          .catch(err => {
+            console.log(err);
+          })
+        })
+      })
+      .catch(err => {
+        console.log(err);
+      })
+    }
+  }
+
+  // Causes state to change => component updates
+  searchUser = userName => {
+    this.setState({
+      mainUserName: userName
+    })
+  }
 
   render(){
     return(
       <div className='App'>
-        <UserCard user={this.state.users}/>
-        {this.state.followers && this.state.followers.map(user => {
-          return <UserCard key={user.id} user={user}/>
-        })}
+        <header>
+          <h1>GitHub Users</h1>
+        </header>
+        <UserForm searchUser={this.searchUser}/>
+        <div className='card-container'>
+          <UserCard user={this.state.mainUser}/>
+          {this.state.followers && this.state.followers.map(user => {
+            return <UserCard key={user.id} user={user}/>
+          })}
+        </div>
       </div>
     );
   }
