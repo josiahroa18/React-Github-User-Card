4bc14be2050e2a5f5e513d2ac7e96efa3b71a5a9
Josiah Roa <josiahroa18@gmail.com>
2020-03-10 12:35:22 -0600

    Styled form component

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -8,43 +8,43 @@ class App extends React.Component{
   constructor(){
     super();
     this.state={
-      mainUserName: 'josiahroa18',
+      mainUserName: '',
       mainUser: {},
       followers: []
     }
   }
 
-  componentDidMount(){
-    // My Github Data
-    axios.get(`https://api.github.com/users/${this.state.mainUserName}`)
-    .then(res => {
-      this.setState({
-        mainUser: res.data
-      })
-    })
-    .catch(err => {
-      console.log(err);
-    })
+  // componentDidMount(){
+  //   // My Github Data
+  //   axios.get(`https://api.github.com/users/${this.state.mainUserName}`)
+  //   .then(res => {
+  //     this.setState({
+  //       mainUser: res.data
+  //     })
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //   })
 
-    // Followers Data
-    axios.get(`https://api.github.com/users/${this.state.mainUserName}/followers`)
-    .then(res => {
-      res.data.forEach(user => {
-        axios.get(`https://api.github.com/users/${user.login}`)
-        .then(res => {
-          this.setState({
-            followers: [...this.state.followers, res.data]
-          })
-        })
-        .catch(err => {
-          console.log(err);
-        })
-      })
-    })
-    .catch(err => {
-      console.log(err);
-    })
-  }
+  //   // Followers Data
+  //   axios.get(`https://api.github.com/users/${this.state.mainUserName}/followers`)
+  //   .then(res => {
+  //     res.data.forEach(user => {
+  //       axios.get(`https://api.github.com/users/${user.login}`)
+  //       .then(res => {
+  //         this.setState({
+  //           followers: [...this.state.followers, res.data]
+  //         })
+  //       })
+  //       .catch(err => {
+  //         console.log(err);
+  //       })
+  //     })
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //   })
+  // }
 
   // Runs when input has changed
   componentDidUpdate(prevProps, prevState){
@@ -101,7 +101,7 @@ class App extends React.Component{
         </header>
         <UserForm searchUser={this.searchUser}/>
         <div className='card-container'>
-          <UserCard user={this.state.mainUser}/>
+          {this.state.mainUserName && <UserCard user={this.state.mainUser}/>}
           {this.state.followers && this.state.followers.map(user => {
             return <UserCard key={user.id} user={user}/>
           })}
